e3712c0f7e24ac820a1a0d4b2e9ce55e83fb9b06
korimse <korimse@gmail.com>
2021-09-28 23:09:56 +0900

    feat: SchedulerConfig

---
+++ src/main/java/com/cloneCoin/analysis/config/scheduler/SchedulerConfig.java
@@ -0,0 +1,25 @@
+package com.cloneCoin.analysis.config.scheduler;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+@Log4j2
+@Configuration
+public class SchedulerConfig implements SchedulingConfigurer {
+    private final int POOL_SIZE = 10;
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+
+        threadPoolTaskScheduler.setPoolSize(POOL_SIZE);
+        threadPoolTaskScheduler.setThreadNamePrefix("my-scheduled-task-pool-");
+        threadPoolTaskScheduler.initialize();
+
+        scheduledTaskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
+//        log.info("Current Thread : {}", Thread.currentThread().getName());
+    }
+}
